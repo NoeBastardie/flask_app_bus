e7be525a5cdad6a55cee901ca96686e450c81a1c
noebastrdie <noe.bastardie@elevate-agency.com>
2022-12-21 11:06:42 +0100

    app creation with bq extract

--- app.py
+++ app.py
@@ -1,40 +1,22 @@
 from flask import Flask
-import http.client, urllib.request, urllib.parse, urllib.error, base64
-import json
 import pandas as pd
-from pandas import json_normalize
-import numpy as np
-
 import geocoder
 import folium
-from folium import Map, Marker, GeoJson, LayerControl
+from folium import Map, Marker
+from google.cloud import bigquery
 
-API_KEY = 'e13626d03d8e4c03ac07f95541b3091b'
+bigquery_Client = bigquery.Client.from_service_account_json('/root/bus_project/bus_app/midyear-cursor-371415-a5bbee084fb8 copy.json')
+
+selectQuery = """SELECT * FROM midyear-cursor-371415.bus_dataset.bus_pos_w_incident"""
+df = bigquery_Client.query(selectQuery).to_dataframe()
 
 def bus_pos_extract():
-    headers = {
-        # Request headers
-        'api_key': API_KEY,
-    }
-
-    params = urllib.parse.urlencode({
-        # Request parameters
-        'Lat': '{number}',
-        'Lon': '{number}',
-        'Radius': '{number}',
-    })
-
-    conn = http.client.HTTPSConnection('api.wmata.com')
-    conn.request("GET", "/Bus.svc/json/jBusPositions?%s" % params, "{body}", headers)
-    response = conn.getresponse()
-    data = response.read()
-    data_str = data.decode()
-    data_dict = json.loads(data_str)
-    return (data_dict)
-
-def bus_pos_transform(data_dict: dict):
-    df = json_normalize(data_dict["BusPositions"])
-    return(df)
+    selectQuery = """SELECT * FROM midyear-cursor-371415.bus_dataset.bus_pos_w_incident"""
+    df = bigquery_Client.query(selectQuery).to_dataframe()
+    def coordinate_split(coordiante_value):
+        return coordiante_value.split(' ')
+    df['bus_coordinate'] = df['bus_coordinate'].apply(coordinate_split)
+    return df
 
 app = Flask(__name__)
 
@@ -46,10 +28,10 @@ def home():
     bus_map = Map(location=latlng,
                 zoom_start=13)
     bus_map.add_child(Marker(location=latlng, popup=loc.address, icon = folium.Icon(color = 'blue')))
-    df = bus_pos_transform(bus_pos_extract())
+    df = bus_pos_extract()
     for i in range(df.shape[0]):
-        folium.features.RegularPolygonMarker(location = [df['Lat'][i], df['Lon'][i]],
-                                            popup = 'Route %s to %s' % (df['RouteID'], df['TripHeadsign']),
+        folium.features.RegularPolygonMarker(location = [df['bus_coordinate'][i][0], df['bus_coordinate'][i][1]],
+                                            # popup = 'Route %s to %s' % (df['RouteID'], df['TripHeadsign']),
                                             number_of_sides = 3,
                                             radius = 10,
                                             weight = 1,
